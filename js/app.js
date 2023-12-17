3a279474994d9d945d4171031a9825a7c0841a0b
haker <whitehaker010@gmail.com>
2023-12-17 15:53:00 +0500

    main

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
+gsap.registerPlugin(ScrollTrigger);
 gsap.from(".header-nav", {
     y:-100,
     skewY:-10,
     duration:1
 });
+
+gsap.from('.banner-img-box', {
+    // y:-100,
+    // duration:1,
+    scale:10,
+    ease:"ease",
+    ScrollTrigger: {
+        trigger:'.img1',
+        scrub:1,
+        start: "top top",
+        end:"bottom",
+        pin:true
+    }
+})
