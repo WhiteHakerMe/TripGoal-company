d0f011542e2efac4026d989e9cafc010d2a0f741
haker <whitehaker010@gmail.com>
2023-12-21 23:43:56 +0500

    main

--- js/app.js
+++ js/app.js
@@ -1,33 +1,36 @@
+gsap.registerPlugin(ScrollTrigger);
+gsap.from(".i",{
+    y: -50,
+    opacity: 0,
+    fixed:true,
+    duration: 1.5,
+    scrollTrigger: {
+        trigger: ".i",
+        start: "top 70%",
+        end:"+=30",
+        toggleActions: "restart  reset",
+    }
+});
 gsap.from(".header-nav", {
     y: -100,
     skewY: -10,
     duration: 1
 });
 
-gsap.registerPlugin(ScrollTrigger);
+
 
 // Animatsiya boshlanishi
 gsap.from("#section1", {
     opacity: 0,
     x: -500,
-    duration: 2
+    duration: 2,
     
+
 });
 gsap.from('.banner-info', {
-    opacity:0,
-    x:500,
-    duration:2,
+    opacity: 0,
+    x: 500,
+    duration: 2,
 });
 
-const elment = document.querySelector('.find');
-const elment2 = document.querySelectorAll('find-info')
-
-window.addEventListener('scroll', () => {
-    if (scrollY > elment.offsetTop - elment.clientHeight / 2) {
-        elment2.forEach((el) => {
-            el.style.transform = `translateY(0)`
-            el.style.opacity = `1`
-        })
-    }
-})
 
